fd5a277381b72e2f025637c32c8ed11c40323ff8
Serhii Pylypchuk <pylypchukserhii1991@gmail.com>
2024-06-25 20:14:05 +0400

    [dev] style refactoring

--- src/ToDo.svelte
+++ src/ToDo.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { onMount } from "svelte";
+    import { onMount, onDestroy } from "svelte";
     import { ToDo, Toolbar } from "@dhx/trial-todolist";
     import "@dhx/trial-todolist/dist/todo.css";
     
@@ -7,23 +7,30 @@
     export let tasks;
     export let projects;
 
-    let toolbar_container;
-    let todo_container;
+    let toolbar_container, todo_container;
+    let todo, toolbar;
 
     onMount(() => {
-        const todo = new ToDo(todo_container, {
+        todo = new ToDo(todo_container, {
             users, 
             tasks, 
             projects,
             // other configuration properties
         })
 
-        new Toolbar(toolbar_container, {
+        toolbar = new Toolbar(toolbar_container, {
             api: todo.api,
             // other configuration properties
         })
     });
+
+    onDestroy(() => {
+        todo.destructor();
+        toolbar.destructor();
+    });
 </script>
 
-<div bind:this={toolbar_container}></div>
-<div bind:this={todo_container} style="width: 100%; height: 100%;"></div>
+<div class="component_container">
+    <div bind:this={toolbar_container}></div>
+    <div bind:this={todo_container} style="height: calc(100% - 56px);"></div>
+</div>
